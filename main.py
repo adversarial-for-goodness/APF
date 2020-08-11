147efe6f6dd19ca2203db168668ab441234f0655
Lan <lanzhang1107@gmail.com>
2020-08-11 10:31:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ parser.add_argument('--config_path', type=str, default='./configs/config_ms1m_10
 parser.add_argument('--insightface_model_path', type=str, default='/data/jiaming/code/InsightFace-tensorflow/model/ms1m/best-m-334000', help='model path')
 parser.add_argument('--val_data', type=str, default='', help='val data, a dict with key as data name, value as data path')
 parser.add_argument('--train_mode', type=int, default=0, help='whether set train phase to True when getting embds. zero means False, one means True')
-# parser.add_argument('--target_far', type=float, default=1e-3, help='target far when calculate tar')
 
 parser.add_argument('--mobilefacenet_model_path', type=str, default='/data/jiaming/code/InsightFace-tensorflow/model/mobilefacenet/MobileFaceNet_TF.ckpt')
 parser.add_argument('--train_data', type=str, default='/data/jiaming/datasets/faces/faces_emore/100000.pkl')
